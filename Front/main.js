b73f8db7ac683907881d02a6f8cd71bee526ec68
Sergey Kurak <smkurak@gmail.com>
2022-10-18 19:18:46 +0300

    ff

--- Front/main.js
+++ Front/main.js
@@ -32,6 +32,7 @@ const view = new View({
 //  - один растровый слой
 //  - источник: открытые карты OSM  
 const map = new Map({
+  //control:null,
   target: 'map',
   layers: [
     new TileLayer({
@@ -41,6 +42,90 @@ const map = new Map({
   ],
   view: view,
 });
+
+/*
+  Функция LonTest проверят значение долготы на корректность
+  Входные папраметры: lon - долгота
+  Возвращаемое значение: строка символов
+     "Longitude is not a number" - lon содержит недопустимые символы
+     "Longitude out of range (-180, 180)" - значение lon находится за пределами допустимого диапазона 
+     "OK" - lon содержит корректное значение
+*/
+function LonTest(lon)
+{
+  var numberLon = Number(lon);
+  if(Number.isNaN(numberLon) == true)
+  {
+     return "Longitude is not a number";
+  }
+  
+  if(numberLon < -180 || numberLon > 180)
+  {
+    return "Longitude out of range (-180, 180)";  
+  }
+  else
+  {
+    return "OK";  
+  }
+}
+
+/*
+  Функция LatTest проверят значение широты на корректность
+  Входные папраметры: lat - широта
+  Возвращаемое значение: строка символов
+     "Latitude is not a number" - lat содержит недопустимые символы
+     "Latitude out of range (-90, 90)" - значение lat находится за пределами допустимого диапазона 
+     "OK" - lat содержит корректное значение
+*/
+function LatTest(lat)
+{
+  var numberLat = Number(lat);
+  if(Number.isNaN(numberLat) == true)
+  {
+    return "Latitude is not a number";
+  }
+  
+  if(numberLat < -90 || numberLat > 90)
+  {
+    return "Latitude out of range (-90, 90)";  
+  }
+  else
+  {
+    return "OK";  
+  }
+}
+
+/*
+  Функция PointTest проверят значение количества точек в ортодромии
+  Входные папраметры: nPoint - количество точек
+  Возвращаемое значение: строка символов
+     "Number of points is not integer number" - nPoint содержит недопустимые символы
+     "Number of points of ange (5, 10 090)" - значение nPoint находится за пределами допустимого диапазона 
+     "OK" - nPoint содержит корректное значение
+*/
+function PointTest(nPoint)
+{
+  var n = Number(nPoint);
+  if(Number.isInteger(n) == false)
+  {
+    return "Number of points is not integer number";
+  }
+  if(String(nPoint).indexOf('.') >= 0)
+  {
+    return "Number of points is not integer number";
+  }
+  
+  if(n < 5 || n > 10000)
+  {
+    return "Number of points of ange (5, 10 000)";  
+  }
+  else
+  {
+    return "OK";  
+  }
+}
+
+
 
 //Функция для занесения в список обработчиков ссылки на функцию callback для обрабобки события onClick  на элементе пользовательского интерфейса с идентификатором id 
 function onClick(id, callback) {
@@ -52,19 +137,56 @@ onClick('start', function () {
 
 //1. Очистка предыдущих изображений точек ИПМ и КПМ и полилинии ортодромии путем удаления этих слоев из карты
 //   (если они есть)  
-var mylayers =   map.getLayers();
-if(mylayers.getLength()>1)
-{ 
-    mylayers.removeAt(1);
-    mylayers.removeAt(1);
+var mylayers;
+while(true) 
+{
+   mylayers =   map.getLayers();
+   if(mylayers.getLength()<2)  break;
+   mylayers.removeAt(1);
 }  
 
-//2.Формированиек строки GET запAзапроса на расчетный Python-сервер  
+//2.Формированиек строки GET запроса на расчетный Python-сервер  
+var testResult;
+
 var lon1 = document.getElementById('lon1').value;
+testResult = LonTest(lon1);
+if(testResult != "OK")
+  {
+    window.alert("LON1: " + testResult);
+    return;
+  }
+
 var lat1 = document.getElementById('lat1').value;
+testResult = LatTest(lat1);
+if(testResult != "OK")
+  {
+    window.alert("LAT1: " + testResult);
+    return;
+  }
+
 var lon2 = document.getElementById('lon2').value;
+testResult = LonTest(lon2);
+if(testResult != "OK")
+  {
+    window.alert("LON2: " + testResult);
+    return;
+  }
+
 var lat2 = document.getElementById('lat2').value;
+testResult = LatTest(lat2);
+if(testResult != "OK")
+  {
+    window.alert("LAT2: " + testResult);
+    return;
+  }
+
 var n = document.getElementById('npoint').value;
+testResult = PointTest(n);
+if(testResult != "OK")
+  {
+    window.alert("NPt: " + testResult);
+    return;
+  }
 
 var req = "http://127.0.0.1:8080/?lon1=" + lon1 + "&lat1=" + lat1 + "&lon2=" + lon2 + "&lat2=" + lat2 + "&n=" + n 
 
@@ -130,7 +252,7 @@ $.ajax({url:req,
         for(i=0; i<ort.length; i++ )
          {
              //Формируем текстовую строку по данным очередной точки ортодромии и присоединяем ее к уже имеющейся строке в элемента вывода out
-             out.value = out.value + '[' + ort[i][0] + "," + ort[i][1]+"]  ";
+             out.value = out.value + '[' + ort[i][0] + ", " + ort[i][1]+"]  ";
          }
       
        })
@@ -139,3 +261,7 @@ $.ajax({url:req,
  .catch(function (err) { console.log('err')});
 
 });
+
+
+
+
